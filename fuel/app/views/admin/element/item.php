bef0902037ce18c8d53079542268a8bc37ee0868
codicecc <gnucms@gmail.com>
2017-06-21 17:34:45 +0200

    Elements Models relationship viewing

--- fuel/app/views/admin/element/item.php
+++ fuel/app/views/admin/element/item.php
@@ -2,12 +2,18 @@
 	$input_id = 'element_list_'.$element->id;
 ?>
 	<li>
+		<?php
+			$checked=false;
+			foreach($element->models as $element_model ) {
+				if($model->id==$element_model->id)$checked=true;
+			}
+		?>
 		<input
 			type="checkbox"
 			autocomplete="off"
 			id="<?php echo $input_id; ?>"
 			data-element_id="<?php echo $element->id; ?>"
-			<?php //echo $element->status ? 'checked' : ''; ?>
+			<?php echo $checked ? 'checked' : ''; ?>
 			>
 		<label for="<?php echo $input_id; ?>">
 			<?php echo $element->name; ?> <a target="_blank" href="/admin/detail/index/<?php echo $element->id;?>"><i class="fa fa-external-link fa-fw"></i></a>
--- fuel/app/views/admin/element/list.php
+++ fuel/app/views/admin/element/list.php
@@ -1,9 +1,8 @@
 
 <ul id="element_list" data-project_id="<?php echo $model->id; ?>">
 <?php 
-//foreach ($model->elements as $element) {
 foreach (Model_Element::find('all') as $element) {
 ?>
-	<?php echo render('admin/element/item', array('element' => $element)); ?>
+	<?php echo render('admin/element/item', array('element' => $element, 'model' => $model)); ?>
 <?php } ?>
 </ul>
