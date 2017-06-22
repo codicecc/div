8cd25ff1b62f07530ddaa227d75e148b8f92a307
codicecc <gnucms@gmail.com>
2017-06-22 17:57:50 +0200

    Dashboard

--- fuel/app/views/admin/dashboard.php
+++ fuel/app/views/admin/dashboard.php
@@ -1,4 +1,9 @@
 <div class="row">
+	<div class="col-md-12">
+		<div class="panel-heading">
+			<h2>Gestione Misure, Taglie, Studenti e Scuole</h2>
+		</div>
+	</div>				
 	<div class="col-lg-3 col-md-6">
 		<div class="panel panel-primary">
 			<div class="panel-heading">
@@ -66,6 +71,103 @@
 		</div>
 	</div>
 	<div class="col-lg-3 col-md-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-support fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge">13</div>
+						<div>Argomenti in Guida!</div>
+					</div>
+				</div>
+			</div>
+			<a href="/admin/help">
+				<div class="panel-footer">
+					<span class="pull-left">View Details</span>
+					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+					<div class="clearfix"></div>
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="panel-heading">
+			<h2>Gestione Elementi, Dettagli e Modelli</h2>
+		</div>
+	</div>				
+	<div class="col-lg-3 col-md-6">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-tag fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge"><?php echo Helper::count("elements")?></div>
+						<div><?php echo __("admin.Elements");?>!</div>
+					</div>
+				</div>
+			</div>
+			<a href="/admin/element">
+				<div class="panel-footer">
+					<span class="pull-left"><?php echo __("admin.Details");?></span>
+					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+					<div class="clearfix"></div>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-6">
+		<div class="panel panel-green">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-dot-circle-o fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge"><?php echo Helper::count("details")?></div>
+						<div><?php echo __("admin.Details");?>!</div>
+					</div>
+				</div>
+			</div>
+			<a href="/admin/detail">
+				<div class="panel-footer">
+					<span class="pull-left"><?php echo __("admin.Details");?></span>
+					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+					<div class="clearfix"></div>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class="col-lg-3 col-md-6">
+		<div class="panel panel-yellow">
+			<div class="panel-heading">
+				<div class="row">
+					<div class="col-xs-3">
+						<i class="fa fa-database fa-5x"></i>
+					</div>
+					<div class="col-xs-9 text-right">
+						<div class="huge"><?php echo Helper::count("models")?></div>
+						<div><?php echo __("admin.Models");?>!</div>
+					</div>
+				</div>
+			</div>
+			<a href="/admin/model">
+				<div class="panel-footer">
+					<span class="pull-left"><?php echo __("admin.Details");?></span>
+					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+					<div class="clearfix"></div>
+				</div>
+			</a>
+		</div>
+	</div>
+<!--
+	<div class="col-lg-3 col-md-6">
 		<div class="panel panel-red">
 			<div class="panel-heading">
 				<div class="row">
@@ -87,4 +189,5 @@
 			</a>
 		</div>
 	</div>
+-->
 </div>
